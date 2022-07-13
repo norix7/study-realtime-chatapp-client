7aa41cab5f7e2ec4a6e3d04c9082170bbfcf9dec
norix7 <norix7@gmail.com>
2022-07-13 16:40:28 +0900

    change index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import { useState } from 'react';
 // サーバー側とクライアント側の両方で、第2引数にpathを同値で設定する必要がある。
 const socket = io("https://study-realtime-chatapp-server.vercel.app", {
   // path: "/socket.io/",
-  withCredentials: true
+  // withCredentials: true
 });
 
 /*
